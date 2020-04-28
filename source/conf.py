f73e642fd318fab4a205c5b6c187307686c0f91e
m4sterbunny <m4sterbunny@gmail.com>
2020-04-28 11:44:45 +0200

    clearout

--- source/conf.py
+++ source/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Zoom Walkthrough'
+project = 'Demo Walkthrough'
 copyright = '2020, Harrie'
 author = 'Harrie'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -53,7 +53,3 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-
-# The master toctree document.
-master_doc = 'index'
